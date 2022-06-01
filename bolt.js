77ac8febc16b1798b93693ae8a012202a8f21906
kusupon <kusupon0628@gmail.com>
2022-06-01 17:08:52 +0900

    first commit

---
+++ bolt.js
@@ -0,0 +1,40 @@
+'use strict';
+const bolt = require('@slack/bolt');
+const dotenv = require('dotenv');
+dotenv.config();
+const todo = require('todo');
+
+const app = new bolt.App({
+    token: process.env.SLACK_BOT_TOKEN,
+    appToken: process.env.SLACK_APP_TOKEN,
+    socketMode: true,
+    logLevel: 'debug'
+});
+
+app.message(/add (.+)/i, ({context, say}) => {
+    const task = context.matches[1].trim();
+    todo.add(task);
+    say(`追加しました: ${task}`);
+});
+
+app.message(/done (.+)/i, ({context, say}) => {
+    const task = context.matches[1].trim();
+    todo.done(task);
+    say(`完了にしました: ${task}`);
+});
+
+app.message(/del (.+)/i, ({context, say}) => {
+    const task = context.matches[1].trim();
+    todo.del(task);
+    say(`削除しました: ${task}`);
+});
+
+app.message(/^list/i, ({context, say}) => {
+    say(todo.list().join('\n'));
+});
+
+app.message(/^donelist/i, ({context, say}) => {
+    say(todo.donelist().join('\n'));
+});
+
+app.start();
---
+++ todo/index.js
@@ -0,0 +1,83 @@
+'use strict';
+// { name: タスクの文字列, state: 完了しているかどうかの真偽値}
+const tasks = [];
+
+/**
+ * TODO を追加する
+ * @param {string} taskName
+ */
+
+function add(taskName) {
+    tasks.push({ name: taskName, state: false});
+}
+
+/**
+ * タスクが完了したかどうかが含まれるオブジェクトを受け取り、完了したかを返す
+ * @param {object} taskAndIsDonePair
+ * @return {boolean} 完了したかどうか
+ */
+
+function isDone(taskAndIsDonePair) {
+    return taskAndIsDonePair.state;
+}
+
+/**
+ * タスクと完了したかどうかが含まれるオブジェクトを受け取り、完了していないかを返す
+ * @param {object} taskAndIsDonePair
+ * @return {boolean} 完了していないかどうか
+ */
+function isNotDone(taskAndIsDonePair) {
+    return !isDone(taskAndIsDonePair);
+}
+
+/**
+ *  TODOの一覧の配列を取得する
+ *  @return {array}
+ */
+
+function list() {
+    return tasks.filter(isNotDone).map(task => task.name);
+}
+
+/**
+ * TODOを完了状態にする
+ * @param {string} taskName
+ */
+
+function done(taskName) {
+    const indexFound = tasks.findIndex(task => task.name === taskName);
+    if (indexFound !== -1) {
+        tasks[indexFound].state = true;
+    }
+}
+
+/**
+ * 完了済のタスクの一覧を取得する
+ * @return {array}
+ */
+
+function donelist() {
+    return tasks
+        .filter(task => task.state)
+        .map(task => task.name);
+}
+
+/**
+ *  項目を削除する
+ * @param {string} taskName
+ */
+
+function del(taskName) {
+    const indexFound = tasks.findIndex(task => task.name === taskName);
+    if (indexFound !== -1) {
+        tasks.splice(indexFound, 1);
+    }
+}
+
+module.exports = {
+    add,
+    list,
+    done,
+    donelist,
+    del
+};
